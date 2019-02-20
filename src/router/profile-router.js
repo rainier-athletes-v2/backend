2eac2e8216abb8b50d1426a98efdef085972ce35
Tracy Williams <selpilot@gmail.com>
2019-02-20 13:38:21 -0800

    removed packages and source files not being used

--- src/router/profile-router.js
+++ src/router/profile-router.js
@@ -1,96 +1,11 @@
 import { Router } from 'express';
 import HttpErrors from 'http-errors';
 import superagent from 'superagent';
-import Profile from '../model/profile';
 import bearerAuthMiddleware from '../lib/middleware/bearer-auth-middleware';
-import logger from '../lib/logger';
-import * as soql from './sf-soql-queries';
+// import logger from '../lib/logger';
+import * as soql from '../lib/sf-soql-queries';
 
 const profileRouter = new Router();
-
-profileRouter.post('/api/v1/profiles', bearerAuthMiddleware, (request, response, next) => {
-  logger.log(logger.INFO, `.post /api/v1/profiles req.body: ${request.body}`);
-  Profile.init()
-    .then(() => {
-      return new Profile(request.body).save();
-    })
-    .then((profile) => {
-      logger.log(logger.INFO, `POST PROFILE ROUTER: new profile created with 200 code, ${JSON.stringify(profile)}`);
-      return response.json(profile).status(200);
-    })
-    .catch((err) => {
-      // err.code === 11000 is conflict on duplicate primaryEmail. If found, reactivate profile.
-      if (err.code === 11000) return Profile.findOne({ primaryEmail: request.body.primaryEmail });
-      return next(err);
-    })
-    .then((result) => {
-      const keys = Object.keys(request.body);
-      for (let i = 0; i < keys.length; i++) {
-        result[keys[i]] = request.body[keys[i]];
-      }
-      result.active = true;
-      return result.save();
-    })
-    .then((result) => {
-      return response.json(result).status(200);
-    })
-    .catch(next);
-
-  return undefined;
-});
-
-profileRouter.get('/api/v1/profiles', bearerAuthMiddleware, (request, response, next) => {
-  if (request.query.id && request.profile.role !== 'admin' && request.profile.role !== 'mentor') {
-    return next(new HttpErrors(401, 'User not authorized to query by id.', { expose: false }));
-  }
-  if (request.query.id) {
-    Profile.init()
-      .then(() => {
-        Profile.findById(request.query.id)
-          .then((profile) => {
-            return response.json(profile);
-          })
-          .catch(next);
-      });
-    return undefined;
-  }
-
-  if (Object.keys(request.query).length > 0) {
-    Profile.init()
-      .then(() => {
-        return Profile.find(request.query);
-      })
-      .then((requestedPropReturn) => {
-        return response.json(requestedPropReturn);
-      })
-      .catch(next);
-    return undefined;
-  }
-
-  if (request.profile.role === 'admin') {
-    Profile.init()
-      .then(() => {
-        return Profile.find();
-      })
-      .then((profiles) => {
-        return response.json(profiles);
-      })
-      .catch(next);
-    return undefined;
-  }
-
-  Profile.init()
-    .then(() => {
-      Profile.findById(request.profile._id.toString())
-        .then((profile) => {
-          delete profile.role;
-          return response.json(profile);
-        });
-      return undefined;
-    })
-    .catch(next);
-  return undefined;
-});
 
 profileRouter.get('/api/v2/profiles/me', bearerAuthMiddleware, (request, response, next) => {
   if (request.profile) {
@@ -251,48 +166,6 @@ profileRouter.get('/api/v2/profiles/myStudents', bearerAuthMiddleware, async (re
     return response.json(studentContacts);
   }
   return next(new HttpErrors(401, 'User not authorized to query by id.', { expose: false }));
-});
-
-
-// update route
-profileRouter.put('/api/v1/profiles', bearerAuthMiddleware, (request, response, next) => {
-  if (!Object.keys(request.body).length) return next(new HttpErrors(400, 'PUT PROFILE ROUTER: Missing request body', { expose: false }));
-
-  Profile.init()
-    .then(() => {
-      return Profile.findOneAndUpdate({ _id: request.body._id }, request.body, { runValidators: true });
-    })
-    .then((profile) => {
-      return Profile.findOne(profile._id);
-    })
-    .then((profile) => {
-      response.json(profile);
-    })
-    .catch(next);
-  return undefined;
-});
-
-profileRouter.delete('/api/v1/profiles', bearerAuthMiddleware, (request, response, next) => {
-  if (request.profile.role !== 'admin') return next(new HttpErrors(401, 'User not authorized to query by id.', { expose: false }));
-  if (!request.query.id) return next(new HttpErrors(400, 'Bad delete request. Missing id query.', { expose: false }));
-  Profile.init()
-    .then(() => {
-      return Profile.findById(request.query.id);
-    })
-    .then((profile) => {
-      if (!profile) return next(new HttpErrors(404, 'Error locating profile for inactivation', { expose: false }));
-      profile.active = false;
-      return profile.save();
-    })
-    .then((result) => {
-      return response.json(result).status(200);
-    })
-    .catch((err) => {
-      logger.log(logger.ERROR, 'DELETE PROFILE ROUTER: non-fatal errors deactivating profile');
-      return next(new HttpErrors(404, `Error deactivating profile: ${err}`, { expose: false }));
-      // return response.status(200);
-    });
-  return undefined;
 });
 
 export default profileRouter;
