278fa6379249c449dc24f7522ebe1b8468d69008
Tracy Williams <selpilot@gmail.com>
2019-03-15 12:08:15 -0700

    working on refresh token handling

--- src/router/bc-oauth-router.js
+++ src/router/bc-oauth-router.js
@@ -12,83 +12,38 @@ const bcOAuthRouter = new Router();
 const retrieveBasecampInfo = async (bcResponse, next) => {
   const accessToken = bcResponse.body.access_token;
   const refreshToken = bcResponse.body.refresh_token;
-  console.log('bcResponse.body', bcResponse.body);
+
   // we have credentials now. Need to drill down to user's Contact record to verify their role(s).
   // first, use id url to retrieve their user_id and sobjects url
-  const accountsUrl = 'https://launchpad.37signals.com/authorization.json';
-  let accountResponse;
+  const authorizationsUrl = 'https://launchpad.37signals.com/authorization.json';
+  let authResponse;
   try {
-    accountResponse = await superagent.get(accountsUrl).set('Authorization', `Bearer ${accessToken}`);
+    authResponse = await superagent.get(authorizationsUrl)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .set({ 'User-Agent': 'Rainier Athletes Mentor Portal (selpilot@gmail.com)' });
   } catch (err) {
-    return next(new HttpErrors(err.status, `Error retrieving id from ${accountsUrl}`, { expose: false }));
+    return next(new HttpErrors(err.status, `Error retrieving info from ${authorizationsUrl}`, { expose: false }));
   }
-  // const ptUpdateUrl = `${sfResponse.body.instance_url}/services/data/v${process.env.SF_API_VERSION}/composite/sobjects`;
-  // const sobjectsUrl = idResponse.body.urls.sobjects.replace('{version}', process.env.SF_API_VERSION);
-  // const queryUrl = idResponse.body.urls.query.replace('{version}', process.env.SF_API_VERSION);
-  // const userId = idResponse.body.user_id;
-  // const userUrl = `${sobjectsUrl}User/${userId}`;
 
-  // // now get user data
-  // let userResponse;
-  // try {
-  //   userResponse = await superagent.get(userUrl).set('Authorization', `Bearer ${accessToken}`);
-  // } catch (err) {
-  //   return next(new HttpErrors(err.status, `Error retrieving User info from ${sobjectsUrl}User/${userId}`, { expose: false }));
-  // }
-
-  // // now get ContactId and retrieve Contact record
-  // const contactId = userResponse.body.ContactId;
-  // const contactUrl = `${sobjectsUrl}Contact/${contactId}`;
-  // let contactResponse;
-  // try {
-  //   contactResponse = await superagent.get(contactUrl).set('Authorization', `Bearer ${accessToken}`);
-  // } catch (err) {
-  //   return next(new HttpErrors(err.status, `Error retrieving Contact info from ${sobjectsUrl}Contact/${contactId}`, { expose: false }));
-  // }
-
-  // // now we can validate user's role as Mentor or Staff
-  // const validUser = contactResponse.body.Mentor__c || contactResponse.body.Staff__c;
-  // if (!validUser) {
-  //   return next(new HttpErrors(401, 'User not authorized.', { expose: false }));
-  // }
-
-  // // user is validated.  Build object for use creating raToken
-  // let userRole = 'unauthorized';
-  // if (contactResponse.body.Staff__c) {
-  //   userRole = 'admin';
-  // } else if (contactResponse.body.Mentor__c) {
-  //   userRole = 'mentor';
-  // }
-
-  // const raTokenPayload = {
-  //   accessToken,
-  //   refreshToken,
-  //   sobjectsUrl,
-  //   queryUrl,
-  //   ptUpdateUrl,
-  //   role: userRole,
-  //   contactId,
-  //   contactUrl,
-  //   userId,
-  //   userUrl,
-  //   firstName: idResponse.body.first_name,
-  //   lastName: idResponse.body.last_name,
-  // };
-
-  return accountResponse.body;
+  const raAccount = authResponse.body.accounts.find(a => a.name.trim() === 'Rainier Athletes') || {};
+  const raTokenPayload = {
+    accessToken,
+    refreshToken,
+    accountUrl: raAccount.href,
+  };
+  console.log('basecamp info:', raTokenPayload);
+  return raTokenPayload;
 };
 
 const sendCookieResponse = (response, tokenPayload) => {
-  const raToken = jsonWebToken.sign(tokenPayload, process.env.SECRET);
+  const raToken = jsonWebToken.sign({ accessToken: tokenPayload.accessToken, accountUrl: tokenPayload.accountUrl }, process.env.SECRET);
   const firstDot = process.env.CLIENT_URL.indexOf('.');
   const domain = firstDot > 0 ? process.env.CLIENT_URL.slice(firstDot) : null;
-  const cookieOptions = { maxAge: process.env.SF_SESSION_TIMEOUT_MINUTES * 60 * 1000 };
+  const cookieOptions = { maxAge: 14 * 24 * 60 * 60 * 1000 }; // two weeks
   if (domain) cookieOptions.domain = domain;
-  response.cookie('RaToken', raToken, cookieOptions);
-  response.cookie('RaUser', Buffer.from(tokenPayload.role)
-    .toString('base64'), cookieOptions);
+  response.cookie('RaBcToken', raToken, cookieOptions);
   const refreshOptions = { maxAge: 5 * 360 * 24 * 60 * 60 * 1000 };
-  response.cookie('RaRefresh', tokenPayload.refreshToken, refreshOptions);
+  response.cookie('RaBcRefresh', tokenPayload.refreshToken, refreshOptions);
   return response.redirect(`${process.env.CLIENT_URL}`);
 };
 
@@ -100,20 +55,23 @@ const dumpAccessToken = (token) => {
 
 bcOAuthRouter.post('/api/v2/oauth/bc', async (request, response, next) => {
   // try using the refresh token
+  // ?type=refresh&refresh_token=your-current-refresh-token&client_id=your-client-id&redirect_uri=your-redirect-uri&client_secret=your-client-secret
   let refreshResponse;
   try {
     refreshResponse = await superagent.post(process.env.SF_OAUTH_TOKEN_URL)
-      .type('form')
-      .send({
-        grant_type: 'refresh_token',
+      .query({
+        type: 'refresh',
         refresh_token: request.body.refresh_token,
-        client_id: process.env.SF_OAUTH_ID,
+        client_id: process.env.BC_OAUTH_ID,
+        client_secret: process.env.BC_CLIENT_SECRET,
+        redirect_uri: `${process.env.API_URL}/oauth/bc`,
       });
   } catch (err) {
     console.log('use of refresh token failed', err);
     return next(new HttpErrors(err.status, 'Using refresh token', { expose: false }));
   }
-  
+  console.log('refresh token response', refreshResponse.body);
+
   dumpAccessToken(refreshResponse.body.access_token);
 
   const tokenPayload = await retrieveBasecampInfo(refreshResponse, next);
@@ -121,7 +79,7 @@ bcOAuthRouter.post('/api/v2/oauth/bc', async (request, response, next) => {
   const raToken = jsonWebToken.sign(tokenPayload, process.env.SECRET);
   // const { refreshToken } = tokenPayload;
   // console.log('oauth post response', { raToken, refreshToken });
-  return response.json({ raToken, raUser: Buffer.from(tokenPayload.role).toString('base64') }).status(200);
+  return response.json(tokenPayload).status(200);
 });
 
 bcOAuthRouter.get('/api/v2/oauth/bc', async (request, response, next) => {
@@ -133,6 +91,7 @@ bcOAuthRouter.get('/api/v2/oauth/bc', async (request, response, next) => {
   let bcTokenResponse;
   try {
     bcTokenResponse = await superagent.post(process.env.BC_OAUTH_TOKEN_URL)
+      .set({ 'User-Agent': 'Rainier Athletes Mentor Portal (selpilot@gmail.com)' })
       .query({
         type: 'web_server',
         client_id: process.env.BC_OAUTH_ID,
@@ -153,7 +112,7 @@ bcOAuthRouter.get('/api/v2/oauth/bc', async (request, response, next) => {
   
   const raTokenPayload = await retrieveBasecampInfo(bcTokenResponse, next);
 
-  return response.json(raTokenPayload);
+  return sendCookieResponse(response, raTokenPayload);
 });
 
 export default bcOAuthRouter;
